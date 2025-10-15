0a106caf854f362574773ce80fc1bae774de4387
Harsh verma <iamharshvrma@gmail.com>
2025-10-15 10:59:39 +0530

    Backend url

--- .history/frontend/src/utils/axiosInstance_20251014204255.js
+++ .history/frontend/src/utils/axiosInstance_20251014204255.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://edonation-drive-backend.onrender.com/api', // Update with your backend URL
+  baseURL: 'http://localhost:5000/api', // Update with your backend URL
   headers: {
     'Content-Type': 'application/json',
   },
